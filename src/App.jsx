05d57af49f703235a7700ccb37d8b2e9569df581
Sohail Momin <zelan9691@gmail.com>
2025-08-07 15:04:45 +0530

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import MainGoal from "./MainGoal";
-
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./Header";
+import { User } from "./User";
 
 function App() {
   return (
     <div>
-    <Header/>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
-        <MainGoal/>
       </main>
+      {/* <User/> */}
     </div>
   );
 }
---
+++ src/User.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const userData = {
+  firstName: "Sohail", // feel free to replace the name value
+  lastName: "Momin", // feel free to replace the name value
+  title: "Instructor", // feel free to replace the title value
+};
+
+// Edit the User component code to output the userData data
+export function User() {
+  return (
+    <div id="user" data-testid="user">
+      <h2>{userData.firstName}</h2>
+      <p>{userData.lastName}</p>
+    </div>
+  );
+}
+
+// DON'T edit the App component code
+function App() {
+  return (
+    <div id="app">
+      <h1>Time to Practice</h1>
+      <p>Welcome on board of this course! You got this 💪</p>
+      {/* <User /> */}
+    </div>
+  );
+}
+
+// export default App;
+export default User;
